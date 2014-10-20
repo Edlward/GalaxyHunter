ec34d2e840027fa545d2e642698b170729fc6c8a
Marco Gulino <marco@gulinux.net>
2014-10-20 22:33:51 +0200

    read and write from lin_guider socket

--- dslr-qt-shooter/dslr_shooter_window.cpp
+++ dslr-qt-shooter/dslr_shooter_window.cpp
@@ -1,11 +1,13 @@
 #include "dslr_shooter_window.h"
 #include "ui_dslr_shooter_window.h"
+#include "linguider.h"
 
 DSLR_Shooter_Window::DSLR_Shooter_Window(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::DSLR_Shooter_Window)
 {
     ui->setupUi(this);
+    LinGuider *linguider = new LinGuider(this);
 }
 
 DSLR_Shooter_Window::~DSLR_Shooter_Window()
--- dslr-qt-shooter/linguider.cpp
+++ dslr-qt-shooter/linguider.cpp
@@ -1,8 +1,35 @@
 #include "linguider.h"
-#include <QtNetwork/QUdpSocket>
+#include <QtNetwork/QLocalSocket>
+#include <stdint.h>
+#include <QDebug>
+
+struct command_header {
+    uint16_t signature;
+    uint16_t command;
+    uint32_t data_length;
+} __attribute__ ((packed));
 
 LinGuider::LinGuider(QObject *parent) :
     QObject(parent)
 {
-    QUdpSocket socket;
+    QLocalSocket socket;
+    socket.connectToServer("/tmp/lg_ss");
+    qDebug() << "socket: " << socket.state();
+    command_header header{2, 1, 0 };
+    QByteArray data;
+    char *header_data = reinterpret_cast<char*>(&header);
+    for(int i=0; i<sizeof(command_header); i++) {
+        qDebug() << "header_data[" << i << "]: " << int(header_data[i]);
+        data.push_back(header_data[i]);
+    }
+
+    qDebug() << "data: " << data;
+    qDebug() << "write: " << socket.write(data);
+    socket.flush();
+    socket.waitForReadyRead();
+    QByteArray answer = socket.read(1024);
+    qDebug() << "Answer: size=" << answer.size() << "; " << answer;
+    for(int i=0; i<answer.size(); i++) {
+        qDebug() << "answer[" << i << "]: " << int(answer[i]);
+    }
 }
