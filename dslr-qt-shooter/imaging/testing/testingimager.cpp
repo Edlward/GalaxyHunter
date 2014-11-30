b25678a6d7906c94fb24f4764bc46a98b6b6fe25
Marco Gulino <marco@gulinux.net>
2014-11-30 11:18:31 +0100

    using testing imager settings (empty for now)

--- dslr-qt-shooter/imaging/testing/testingimager.cpp
+++ dslr-qt-shooter/imaging/testing/testingimager.cpp
@@ -8,9 +8,35 @@
 
 using namespace std;
 
+class TestingImager::Settings : public Imager::Settings {
+public:
+  virtual ComboSetting shutterSpeed() const { return _shutterSpeed; }
+  virtual ComboSetting imageFormat() const { return _imageFormat; }
+  virtual ComboSetting iso() const { return _iso; }
+  virtual void setShutterSpeed(const QString &s) { _shutterSpeed.current = s; }
+  virtual void setImageFormat(const QString &s) { _imageFormat.current = s; }
+  virtual void setISO(const QString &s) { _iso.current = s; }
+  virtual void setManualExposure(qulonglong seconds) { _manualExposure = seconds; }
+  virtual qulonglong manualExposure() const { return _manualExposure; }
+  virtual void setSerialShootPort(const string serialShootPort) { _serialPort = serialShootPort; }
+  virtual string serialShootPort() const { return _serialPort; }
+private:
+  ComboSetting _shutterSpeed;
+  ComboSetting _imageFormat;
+  ComboSetting _iso;
+  qulonglong _manualExposure;
+  string _serialPort;
+};
+
+
 TestingImagerDriver::TestingImagerDriver(QObject *parent) :
     ImagingDriver(parent)
 {
+}
+
+TestingImager::TestingImager() : Imager(), _settings(make_shared<TestingImager::Settings>())
+{
+
 }
 
 
@@ -36,10 +62,5 @@ QImage TestingImager::shoot() const
   qDebug() << "loading image: " << imageFile;
   QImage image(imageFile);
   return image;
-}
-
-shared_ptr< Imager::Settings > TestingImager::settings()
-{
-
 }
 
--- dslr-qt-shooter/imaging/testing/testingimager.h
+++ dslr-qt-shooter/imaging/testing/testingimager.h
@@ -6,14 +6,19 @@
 class TestingImager : public Imager {
   Q_OBJECT
 public:
+  TestingImager();
     virtual QString summary() const { return "---- Testing Imager ----"; }
     virtual QString model() const { return "Testing Imager v.0.0"; }
     virtual QString about() const { return ""; }
-    virtual std::shared_ptr<Settings> settings();
+    class Settings;
+    friend class Settings;
+    virtual std::shared_ptr<Imager::Settings> settings() { return _settings; }
 public slots:
   virtual void connect();
   virtual void disconnect();
   virtual QImage shoot() const;
+private:
+  std::shared_ptr<Imager::Settings> _settings;
 };
 class TestingImagerDriver : public ImagingDriver
 {
